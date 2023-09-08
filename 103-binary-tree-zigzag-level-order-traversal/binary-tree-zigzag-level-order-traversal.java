94f6c0b98a362d4ce566eed85d15a9c913a0ee6a
Atagara Gangadhar <107497796+Gangu-0629@users.noreply.github.com>
2023-09-08 11:53:11 +0530

    Time: 1 ms (90.61%) | Memory: 41.2 MB (62.40%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) 
+    {
+        List<List<Integer>> res=new LinkedList<List<Integer>>();
+        Deque<TreeNode> deque=new LinkedList<TreeNode>();
+        if(root==null)return res;
+        int flag=0;//normal order 0,reverse order 1
+        deque.offer(root);
+        while(!deque.isEmpty())
+        {
+            int size=deque.size();
+            List<Integer> al=new LinkedList<Integer>();
+            if(flag==0)//normal order
+            {
+                for(int i=0;i<size;i++)
+                {
+                    TreeNode poped=deque.pollFirst();
+                    if(poped.left!=null) deque.offerLast(poped.left);
+                    if(poped.right!=null) deque.offerLast(poped.right);
+                    al.add(poped.val);
+                }
+
+                flag=1;
+            }
+            else//reverse order
+            {
+                  for(int i=0;i<size;i++)
+                {
+                    TreeNode poped=deque.pollLast();
+                    if(poped.right!=null) deque.offerFirst(poped.right);
+                    if(poped.left!=null) deque.offerFirst(poped.left);
+                      al.add(poped.val);
+                   
+                }
+                flag=0;
+            }
+            res.add(al);
+        }
+        return res;
+
+    }
+}
