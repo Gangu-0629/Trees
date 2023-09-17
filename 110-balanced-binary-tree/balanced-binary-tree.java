a8666f383c057e068241876a9a1718ba4d97b9d1
Atagara Gangadhar <107497796+Gangu-0629@users.noreply.github.com>
2023-09-17 10:00:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (16.63%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -14,26 +14,21 @@
  * }
  */
 class Solution {
-    Map<TreeNode,Integer> map=new HashMap<>();
+
     public boolean isBalanced(TreeNode node) {
         
-        if(node==null)return true;
-        int left=height(node.left);
-        int right=height(node.right);
-           // System.out.println(node.val+" with height of "+Math.abs(left-right));
-            boolean check=Math.abs(left-right)<=1?true:false;
-        return (check && isBalanced(node.left) && isBalanced(node.right));
+        return height(node)!=-2;
     }
     public int height(TreeNode node){
         if(node==null)
         {
             return -1;
         }
-        if(map.containsKey(node))return map.get(node);
+ 
         int left=height(node.left);
         int right=height(node.right);
-        int hei=Math.max(left,right)+1;
-        map.put(node,hei);
-        return hei;
+    if(left==-2||right==-2)return -2;//unbalanaced due there is unbalanced subtree in left or right
+    if(Math.abs(left-right)>1)return -2;//unbalanced due to height dif
+        return Math.max(left,right)+1;
     }
 }
